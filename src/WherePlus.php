4d7c552db3fb495f019b6b5a27a607f01c29745f
Michael Blair <mhkblair@gmail.com>
2018-01-16 16:15:36 +1100

    Initial commit

---
+++ src/WherePlus.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Blasttech\WherePlus;
+
+use Illuminate\Database\Eloquent\Builder;
+
+interface WherePlus
+{
+    /**
+     * This function adds a where condition for when a $column should be equal to $value, but not equal to a $ignore
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeWhereOrEmptyOrNull(Builder $query);
+
+    /**
+     * Scope a query to only include records where $value in $column
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeWhereInColumn(Builder $query);
+
+    /**
+     * Scope a query to exclude records where $value in $column
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeWhereNotInColumn(Builder $query);
+}
---
+++ src/WherePlusTrait.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Blasttech\WherePlus;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Trait WherePlusTrait
+ *
+ * @method \Illuminate\Database\Eloquent\Builder whereOrEmptyOrNull($field_name, $value = '', $ignore = null)
+ *
+ * @package Blasttech\WherePlus
+ */
+trait WherePlusTrait
+{
+    /**
+     * Add ticks to a table and column
+     *
+     * @param string $column
+     * @return string
+     */
+    private function addTicks($column)
+    {
+        if (preg_match('/^[0-9a-zA-Z\.]*$/', $column)) {
+            return '`' . str_replace(['`', '.'], ['', '`.`'], $column) . '`';
+        } else {
+            return $column;
+        }
+    }
+
+    /**
+     * If $value = '' add where ($column is null or column = '') statement
+     * If $value != '' add where $column = $value statement
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string|array $column
+     * @param string $value
+     * @param string $ignore - if value = ignore, don't search on this column
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeWhereOrEmptyOrNull($query, $column, $value = '', $ignore = null)
+    {
+        if (is_array($column)) {
+            foreach ($column as $where_col => $where_val) {
+                $query->whereOrEmptyOrNull($where_col, $where_val, $ignore);
+            }
+        } else {
+            if (!is_null($ignore) && $value != $ignore) {
+                if ($value == '') {
+                    $query->where(function ($query) use ($column) {
+                        /** @var Builder $query */
+                        $query->where($column, '')
+                            ->orWhereNull($column);
+                    });
+                } else {
+                    $query->where($column, $value);
+                }
+            }
+        }
+
+        return $query;
+    }
+
+    /**
+     * Scope a query to only include records where $value in $column
+     *
+     * @param Builder $query
+     * @param string $column
+     * @param string $value
+     * @return Builder
+     */
+    public function scopeWhereInColumn($query, $column, $value)
+    {
+        return $query->whereRaw('CONCAT(\',\', ' . $this->addTicks($column) . ', \',\') LIKE \'%,' . $value . ',%\'');
+    }
+
+    /**
+     * Scope a query to exclude records where $value in $column
+     *
+     * @param Builder $query
+     * @param string $column
+     * @param string $value
+     * @return Builder
+     */
+    public function scopeWhereNotInColumn($query, $column, $value)
+    {
+        return $query->whereRaw(
+            'CONCAT(\',\', ' . $this->addTicks($column) . ', \',\') NOT LIKE \'%,' . $value . ',%\''
+        );
+    }
+}
